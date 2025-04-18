3504b432b3c4726b547545f921e6ea8090be0e01
Nursultan <nursultan.yekzekov@bildit.co>
2025-04-18 15:48:31 +0200

    fix mobile design issue for Commerce Suite Page

--- app/commerce_suite/page.tsx
+++ app/commerce_suite/page.tsx
@@ -1,14 +1,8 @@
-import BILDIT_CommerceSuite_BG from '../../public/images/commerce_suite/BILDIT_CommerceSuite_BG.png'
-import BILDIT_CommerceSuite_Mobile_BG from '../../public/images/commerce_suite/BILDIT_CommerceSuite_Mobile_BG.png'
-import BILDIT_Commerce_Suite_Why_BG from '../../public/images/commerce_suite/BILDIT_Commerce_Suite_Why_BG.png'
-import BILDIT_Commerce_Suite_Why_Bildit_Mobile_BG from '../../public/images/commerce_suite/BILDIT_Commerce_Suite_Why_Bildit_Mobile_BG .png'
-import BILDIT_Dot_Left_Middle_Effect_BG from '../../public/images/others/BILDIT_Dot_Left_Middle_Effect_BG.png'
-import BILDIT_Dot_Right_Middle_Effect_BG from '../../public/images/others/BILDIT_Dot_Right_Middle_Effect_BG.png'
-import BodyOne from '../components/BodyOne'
-import HeadingOne from '../components/HeadingOne'
-import HeadingTwo from '../components/HeadingTwo'
-import SectionCard, { SectionCardType } from '../components/SectionCard'
-import SubTitleThree from '../components/SubTitleThree'
+import BodyOne from '@/app/components/BodyOne'
+import HeadingOne from '@/app/components/HeadingOne'
+import HeadingTwo from '@/app/components/HeadingTwo'
+import SectionCard, { SectionCardType } from '@/app/components/SectionCard'
+import SubTitleThree from '@/app/components/SubTitleThree'
 import Image from 'next/image'
 
 // @todo: should come from cms
@@ -54,20 +48,22 @@ const CardItems: Array<SectionCardType> = [
 export default function CommerceSuite() {
   return (
     <div className="text-center lg:text-left">
-      <section className="pt-[175px] lg:pb-[100px] px-[16px] lg:px-0">
+      <section className="pt-20 lg:pt-40 lg:pb-25 px-4">
         <Image
-          src={BILDIT_CommerceSuite_BG}
-          className="absolute hidden lg:block top-0 left-0 z-[-1]"
+          src="/images/commerce_suite/BILDIT_CommerceSuite_BG.png"
+          className="absolute hidden lg:block top-0 left-0 -z-1 w-full h-auto"
           alt="BILDIT_CommerceSuite_BG.png"
-          style={{ width: '100%', height: 'auto' }}
+          width={1200}
+          height={0}
         />
         <Image
-          src={BILDIT_CommerceSuite_Mobile_BG}
-          className="absolute block lg:hidden top-0 left-0 z-[-1]"
+          src="/images/commerce_suite/BILDIT_CommerceSuite_Mobile_BG.png"
+          className="absolute lg:hidden top-0 left-0 -z-1 w-full h-auto"
           alt="BILDIT_CommerceSuite_Mobile_BG.png"
-          style={{ width: '100%', height: 'auto' }}
+          width={1200}
+          height={0}
         />
-        <div className="container mx-auto lg:grid grid-cols-2 gap-[60px] items-center">
+        <div className="container mx-auto lg:grid grid-cols-2 gap-15 items-center">
           <div>
             <HeadingOne
               sub1="Commerce"
@@ -75,9 +71,9 @@ export default function CommerceSuite() {
               className1="tracking-[2%]"
               className2="bg-gradient-to-r from-purple-700 via-pink-500 to-pink-400 bg-clip-text text-transparent" //from-[#3B1EED] via-[#ED1E79] via-[#EB67A1] via-[#ED1E79] to-[#ED1E79] bg-clip-text text-transparent
             />
-            <SubTitleThree content="Unleash the Power of Commerce Suite with Our CMS" className="mt-[30px]" />
+            <SubTitleThree content="Unleash the Power of Commerce Suite with Our CMS" className="mt-7" />
             <BodyOne
-              className="text-zinc-600 my-[20px]"
+              className="text-zinc-600 my-5"
               content="Say goodbye to the hassle of juggling multiple tools. Our cutting-edge CMS brings everything you need into one seamless platform, designed for modern e-commerce businesses."
             />
           </div>
@@ -87,6 +83,7 @@ export default function CommerceSuite() {
               alt="BILDIT_CommerceSuite_Intro.png"
               width={604}
               height={493}
+              className="w-full h-auto"
             />
           </div>
         </div>
@@ -96,18 +93,20 @@ export default function CommerceSuite() {
             <div className="relative" key={key}>
               {key === 1 && (
                 <Image
-                  src={BILDIT_Dot_Right_Middle_Effect_BG}
-                  className="hidden lg:block absolute bottom-0 right-0 z-[-1]"
+                  src="/images/others/BILDIT_Dot_Right_Middle_Effect_BG.png"
+                  className="hidden lg:block absolute bottom-0 right-0 -z-1 w-auto h-auto"
                   alt="BILDIT_Dot_Right_Middle_Effect_BG.png"
-                  style={{ width: 'auto', height: 'auto' }}
+                  width={1200}
+                  height={0}
                 />
               )}
               {key === 2 && (
                 <Image
-                  src={BILDIT_Dot_Left_Middle_Effect_BG}
-                  className="hidden lg:block absolute top-50 left-0 z-[-1]"
-                  alt="BILDIT_Dot_Left_Middle_Effect_BG.png.png"
-                  style={{ width: 'auto', height: 'auto' }}
+                  src="/images/others/BILDIT_Dot_Left_Middle_Effect_BG.png"
+                  className="hidden lg:block absolute top-50 left-0 -z-1 w-auto h-auto"
+                  alt="BILDIT_Dot_Left_Middle_Effect_BG.png"
+                  width={1200}
+                  height={0}
                 />
               )}
               <SectionCard item={item} key={key} />
@@ -116,44 +115,47 @@ export default function CommerceSuite() {
         })}
       </section>
 
-      <section className="mt-[90px] lg:mt-[200px] py-12 lg:py-[100px] bg-center px-4 lg:px-0 relative">
+      <section className="mt-20 lg:mt-50 py-12 lg:py-25 bg-center px-4 lg:px-0 relative">
         <Image
-          src={BILDIT_Commerce_Suite_Why_BG}
-          className="hidden lg:block absolute top-0 left-0 z-[-1]"
+          src="/images/commerce_suite/BILDIT_Commerce_Suite_Why_BG.png"
+          className="hidden lg:block absolute top-0 left-0 -z-1 w-full h-full"
           alt="BILDIT_Commerce_Suite_Why_BG.png"
-          style={{ width: '100%', height: '100%' }}
+          width={1200}
+          height={0}
         />
         <Image
-          src={BILDIT_Commerce_Suite_Why_Bildit_Mobile_BG}
-          className="block lg:hidden absolute top-0 left-0 z-[-1]"
+          src="/images/commerce_suite/BILDIT_Commerce_Suite_Why_Bildit_Mobile_BG.png"
+          className="block lg:hidden absolute top-0 left-0 -z-1 w-full h-full"
           alt="BILDIT_Commerce_Suite_Why_Bildit_Mobile_BG"
-          style={{ width: '100%', height: '100%' }}
+          width={1200}
+          height={0}
         />
         <div className="container mx-auto">
-          <HeadingTwo content="Why Choose BILDIT?" className="text-center text-white lg:pt-[40px]" />
+          <HeadingTwo content="Why Choose BILDIT?" className="text-center text-white lg:pt-10" />
           <div className="lg:grid grid-cols-4 justify-center">
-            <div className="py-12 lg:py-[100px] order-2 col-span-2 flex justify-center">
+            <div className="py-12 lg:py-25 order-2 col-span-2 flex justify-center">
               <Image
                 src="/images/commerce_suite/BILDIT_Why_Choose_BILDIT.png"
                 alt="BILDIT_Why_Choose_BILDIT.png"
                 width={600}
-                height={575}
+                height={0}
+                className="w-full h-auto"
               />
             </div>
             <div className="flex flex-col justify-around items-end order-1col-span-1">
-              <p className="font-400 text-[20px] lg:text-[24px] leading-[24px] mt-[44px] lg:leading-[34px] text-white">
+              <p className="font-400 text-xl lg:text-2xl leading-none mt-10 text-white">
                 Future-Ready Technology: Designed to scale with your business and adapt to evolving e-commerce trends.
               </p>
-              <p className="font-400 text-[20px] lg:text-[24px] leading-[24px] mt-[44px] leading-[34px] text-white">
+              <p className="font-400 text-xl lg:text-2xl leading-none mt-10 text-white">
                 Customer-Centric Features: From native checkout to App Clips, we prioritize enhancing your customer
                 experience.
               </p>
             </div>
             <div className="flex flex-col justify-around order-2 col-span-1 text-center lg:text-left">
-              <p className="font-400 text-[20px] lg:text-[24px] leading-[24px] mt-[44px] lg:leading-[34px] text-white">
+              <p className="font-400 text-xl lg:text-2xl leading-none mt-10 text-white">
                 Seamless Integration: A unified platform that effortlessly bridges mobile apps and websites..
               </p>
-              <p className="font-400 text-[20px] lg:text-[24px] leading-[24px] mt-[44px] lg:leading-[34px] text-white">
+              <p className="font-400 text-xl lg:text-2xl leading-none mt-10 text-white">
                 Time-Saving Simplicity: Manage everything from content to commerce in one intuitive platform.
               </p>
             </div>
--- app/components/SectionCard.tsx
+++ app/components/SectionCard.tsx
@@ -18,11 +18,11 @@ interface Props {
 
 const SectionCard: React.FC<Props> = ({ item }) => {
   return (
-    <div className="pt-[90px] lg:pt-[240px] container mx-auto lg:grid grid-cols-2 items-center gap-[60px]">
+    <div className="pt-20 lg:pt-60 container mx-auto lg:grid grid-cols-2 items-center gap-15">
       <div className={`${item.dir === true ? 'order-2' : ''}`}>
         <SubTitleFiveCaps content={item.title} />
         <DisplayOne content={item.head} />
-        <BodyTwo className="mt-[30px]" content={item.content} />
+        <BodyTwo className="lg:mt-7 mb-3" content={item.content} />
       </div>
       <div className={`flex justify-center ${item.dir === true ? 'order-1' : ''}`}>
         <Image src={item.src} alt={item.alt} width={600} height={600} />
