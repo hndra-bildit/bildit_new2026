30c02ea3ab18f9c6c3524b0d2b919aac6e84cfb9
Nursultan <nursultan.yekzekov@bildit.co>
2025-04-25 18:36:12 +0200

    integrate with static Comparison page and cms backend

--- app/comparison/page.tsx
+++ app/comparison/page.tsx
@@ -1,21 +1,10 @@
-import ComparisonTable from '@/app/components/ComparisonTable'
-import DisplayOne from '@/app/components/DisplayOne'
-import Image from 'next/image'
+import { SlotPlaceholder } from '@bildit-platform/nextjs'
 
 export default function Comparison() {
   return (
-    <section className="py-20 lg:pt-40 text-center py-13">
-      <Image
-        src="/images/comparison/BILDIT_Comparison_BG.png"
-        className="top-0 left-0 absolute -z-1 w-full h-auto"
-        alt="BILDIT_Comparison_BG.png"
-        width={1200}
-        height={0}
-      />
-      <div className="container mx-auto  px-4">
-        <DisplayOne content="Content Management System Comparisons" />
-        <ComparisonTable />
-      </div>
-    </section>
+    <>
+      <SlotPlaceholder slotId="comparison-title" />
+      <SlotPlaceholder slotId="comparison-content" />
+    </>
   )
 }
