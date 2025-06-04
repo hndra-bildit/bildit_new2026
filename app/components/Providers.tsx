d126971050a9f0934bfdd0ea3e4f8a396eb95b64
Wes Guirra <wes.guirra@endeken.com>
2025-06-04 10:24:50 -0300

    BWC-1461 - remove extraDependenciesConfig from BilditProvider because Next dependencies are auto injected by the library

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -2,8 +2,6 @@
 import React, { Suspense } from 'react'
 import { BilditProvider } from '@bildit-platform/nextjs'
 import type { BannerType } from '@bildit-platform/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -15,10 +13,6 @@ const Providers: React.FC<ProvidersProps> = ({ children, banners }) => {
     <Suspense>
       <BilditProvider
         banners={banners}
-        extraDependenciesConfig={{
-          Link: { module: Link },
-          Image: { module: Image }
-        }}
       >
         {children}
       </BilditProvider>
