eb111e21a59bd5aeb8d8278d4275602d9ac4e432
Aaqib925 <muhammad.aqib@bildit.co>
2025-10-20 12:50:51 +0500

    fix: build fixes

--- app/commerce-suite/page.tsx
+++ app/commerce-suite/page.tsx
@@ -1,32 +1,10 @@
-import HeroImage from '../components/HeroImage'
 import { SlotPlaceholder } from '@bildit-platform/nextjs'
-import Image from 'next/image'
 
 export default function CommerceSuite() {
   return (
     <div className="text-center lg:text-left">
       <SlotPlaceholder slotId="commerce-suite-title" />
       <SlotPlaceholder slotId="commerce-suite-content" />
-
-      {/* HeroImage Component */}
-      {/* <HeroImage />
-
-      <section className="relative">
-        <Image
-          src="/images/others/BILDIT_Dot_Right_Middle_Effect_BG.png"
-          className="hidden lg:block absolute top-0 right-0 -z-1 w-auto h-auto"
-          alt="BILDIT_Dot_Right_Middle_Effect_BG.png"
-          width={1200}
-          height={0}
-        />
-        <Image
-          src="/images/others/BILDIT_Dot_Left_Middle_Effect_BG.png"
-          className="hidden lg:block absolute bottom-0 left-0 -z-1 w-auto h-auto"
-          alt="BILDIT_Dot_Left_Middle_Effect_BG.png"
-          width={1200}
-          height={0}
-        />
-      </section> */}
     </div>
   )
 }
