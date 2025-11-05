9399d7f6ecdbc8a13c923745c6a81def5d263c2b
Aaqib925 <muhammad.aqib@bildit.co>
2025-11-05 19:09:01 +0500

    fix: commented out navigation from the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,7 +98,7 @@ export default async function RootLayout({
         {/* <BILDITAIPixel /> */}
         <Providers banners={banners}>
           <Header />
-          <Navigation />
+          {/* <Navigation /> */}
           <div>{children}</div>
           <Footer />
         </Providers>
