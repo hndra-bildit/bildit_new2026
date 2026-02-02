c7eee1ede52149e12034b48dcfd92f8c106af8b3
Aaqib925 <muhammad.aqib@bildit.co>
2026-02-02 16:29:09 +0500

    feat: added logs

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default async function RootLayout({
                   console.log('Script injection message received from parent CMS...');
 
                   const script = document.createElement("script");
-                  script.src = "${process.env.NODE_ENV === 'production' ? '/scripts/admin.js' : 'https://bildit-cdn.bilditon.com/cms-client/bildit.min.js'}";
+                  script.src = "${process.env.NODE_ENV !== 'production' ? '/scripts/admin.js' : 'https://bildit-cdn.bilditon.com/cms-client/bildit.min.js'}";
                   console.log('Script source:', script.src);
                   script.onload = function() {
                     console.log('Web script loaded successfully');
